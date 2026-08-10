bdd5d3f124a9e4ae9a1e404e221e903cc54c7205
vely2277 <velymand@gmail.com>
2026-08-10 07:21:01 +0100

    For first deployment

--- server.js
+++ server.js
@@ -139,6 +139,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
